6a5a4929f99b4c3fcf97687b19c502698df02989
andresporras3423 <andresporras3423@gmail.com>
2020-03-28 19:50:02 -0500

    in the root, get most recent article by category

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,11 +1,16 @@
 module ArticlesHelper
     def most_voted_article
-        sql = "select a.id 
+        sql = "select a.id, count(v.article_id) 
                from articles as a 
                inner join votes as v 
-               group by v.article_id 
+               on a.id = v.article_id
+               group by a.id 
                order by count(v.article_id)"
         hash_id=ActiveRecord::Base.connection.execute(sql)
         Article.all.find(hash_id[0]['id'])
+    end
+
+    def recent_article_by_category(category_id)
+        Category.find(category_id).articles.max_by{|ar| ar.created_at}
     end
 end
