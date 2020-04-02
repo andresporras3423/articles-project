cef795b2244e407c880ae81c867458d41eb0f02e
andresporras3423 <andresporras3423@gmail.com>
2020-04-01 21:29:56 -0500

    update type of foreign keys in votes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_27_223606) do
+ActiveRecord::Schema.define(version: 2020_04_02_022659) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -70,8 +70,8 @@ ActiveRecord::Schema.define(version: 2020_03_27_223606) do
   end
 
   create_table "votes", force: :cascade do |t|
-    t.string "user_id"
-    t.string "article_id"
+    t.integer "user_id"
+    t.integer "article_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
